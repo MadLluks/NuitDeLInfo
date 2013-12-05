0925cdb4a869e7153d821732f5f53bb178095315
mHelba <william.chasson@gmail.com>
2013-12-05 11:46:18 +0100

    Test

---
+++ src/CApp.h
@@ -0,0 +1,36 @@
+#ifndef _CAPP_H_
+    #define _CAPP_H_
+ 
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+ 
+class CApp {
+    private:
+        bool Running;
+        SDL_Window* gWindow;
+        SDL_Renderer* gRenderer;
+        SDL_Texture* gTexture;
+ 
+    public:
+        CApp();
+ 
+        int OnExecute();
+ 
+        bool OnInit();
+ 
+        void OnEvent(SDL_Event* Event);
+ 
+        void OnLoop();
+ 
+        void OnRender();
+ 
+        void OnCleanup();
+
+        SDL_Texture* loadTexture(std::string path);
+
+        bool loadMedia();
+};
+ 
+#endif
